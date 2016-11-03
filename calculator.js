ce656cacbe7ac8a0dcfd69cb2d88a9417ed61757
Kinnard <khockenhull@gmail.com>
2016-11-03 18:55:16 -0400

    Put the foreman in charge

---
+++ calculator.js
@@ -0,0 +1,23 @@
+var profit_per_hash;
+var revenue;
+var cost;
+var hashrate //In hashes per second
+var power_consumption;
+var energy_per_hash 
+var electricity_cost;
+var difficulty;
+var block_reward;
+var rent;
+var exchangeRate;
+
+
+function calculate() {
+    profit_per_hash=(block reward/(2^32*difficulty))-cost_per_hash=electricity_cost*energy_per_hash;
+    profit_per_hour=hashrate*profit_per_hash*60*60;
+    profit_per_day=profit_per_hour*24;
+    profit_per_week=profit_per_day*7;
+    profit_per_month=profit_per_day*30.5;
+    
+    energy_per_hash
+    cost_per_hash=electricity_cost*energy_per_hash;
+}
