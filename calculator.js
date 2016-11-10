b65adf0b2cff0933ecd4d0290a0d9810758a5afe
Kinnard <khockenhull@gmail.com>
2016-11-10 13:49:32 -0500

    Linked scriptvariables to form inputs

--- calculator.js
+++ calculator.js
@@ -1,23 +1,48 @@
 var profit_per_hash;
 var revenue;
 var cost;
-var hashrate //In hashes per second
+var hashrate; //In hashes per second
 var power_consumption;
-var energy_per_hash 
+var energy_per_hash;
 var electricity_cost;
 var difficulty;
 var block_reward;
-var rent;
-var exchangeRate;
+var cost_per_hash;
+var profit_per_hour;
+let rent;
+let exchangeRate;
+let analysis={};
 
 
-function calculate() {
-    profit_per_hash=(block reward/(2^32*difficulty))-cost_per_hash=electricity_cost*energy_per_hash;
-    profit_per_hour=hashrate*profit_per_hash*60*60;
-    profit_per_day=profit_per_hour*24;
-    profit_per_week=profit_per_day*7;
-    profit_per_month=profit_per_day*30.5;
+
+function calculate(cb) {
+    console.log("calculating");
+    analysis={
+        cost_per_hash:electricity_cost*energy_per_hash,
+        profit_per_hash:(block_reward/(2^32*difficulty))-cost_per_hash,
+        profit_per_hour:hashrate*this.profit_per_hash*60*60,
+        profit_per_day:this.profit_per_hour*24,
+        profit_per_week:this.profit_per_day*7,
+        profit_per_month:this.profit_per_day*30.5,
+    }
+    if(cb) {cb(null, analysis);} else{return;};                        
+}
+
+function display(){
+    for (element of document.forms.calculatorForm.elements){
+        let elementName=element.id;
+        window[elementName]=element.value;
+    }
+
+    console.log("Refreshing\n");
+    calculate(function(err, analysis){
+        if (err){console.log(`Error: ${err}\n`)}
+
+        for (metric in analysis){
+            console.log(`Metric: ${metric} – ${analysis[metric]}`);
+            document.forms.screenForm.elements[metric].innerHTML = analysis[metric];
+        }
+    });
     
-    energy_per_hash
-    cost_per_hash=electricity_cost*energy_per_hash;
+    
 }
