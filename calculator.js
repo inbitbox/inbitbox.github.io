69320872f08f6ccdb9fe12432c73bb9d2d0defb2
Kinnard <khockenhull@gmail.com>
2016-11-20 16:13:28 -0500

    Added methods for analysis variables adjusted front-end accordingly

--- calculator.js
+++ calculator.js
@@ -15,15 +15,18 @@ let analysis={};
 
 
 
-function calculate(cb) {
+function calculate(block_reward, difficulty, electricity_cost, energy_per_hash, hashrate,cb) {
     console.log("calculating");
+    console.log(`electricity_cost: ${electricity_cost} energy_per_hash: ${energy_per_hash}`);
+    console.log(`difficulty: ${difficulty} block_reward: ${block_reward}`)
     analysis={
         cost_per_hash:electricity_cost*energy_per_hash,
-        profit_per_hash:(block_reward/(2^32*difficulty))-cost_per_hash,
-        profit_per_hour:hashrate*this.profit_per_hash*60*60,
-        profit_per_day:this.profit_per_hour*24,
-        profit_per_week:this.profit_per_day*7,
-        profit_per_month:this.profit_per_day*30.5,
+        get cost_per_hash_p1(){return this.cost_per_hash+1},
+        get profit_per_hash() {return (block_reward/((2**32)*difficulty))-this.cost_per_hash},
+        get profit_per_hour() {return hashrate*this.profit_per_hash*60*60},
+        get profit_per_day() {return this.profit_per_hour*24},
+        get profit_per_week() {return this.profit_per_day*7},
+        get profit_per_month() {return this.profit_per_day*30.5}
     }
     if(cb) {cb(null, analysis);} else{return;};                        
 }
@@ -32,12 +35,13 @@ function display(){
     for (element of document.forms.calculatorForm.elements){
         let elementName=element.id;
         window[elementName]=element.value;
+        console.log(`${elementName}:${element.value}`);
     }
-
     console.log("Refreshing\n");
-    calculate(function(err, analysis){
+    calculate(block_reward, difficulty, electricity_cost, energy_per_hash, hashrate, function(err, analysis){
         if (err){console.log(`Error: ${err}\n`)}
-
+        console.log("Analysis" + JSON.stringify(analysis));
+        console.log("Cost per hash +1: " +analysis.cost_per_hash_p1);
         for (metric in analysis){
             console.log(`Metric: ${metric} – ${analysis[metric]}`);
             document.forms.screenForm.elements[metric].innerHTML = analysis[metric];
